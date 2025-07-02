d3dc4f4e94ea7c16392705e2e042037d34c7bd36
unknown <xeno035>
2025-07-02 12:01:52 +0530

    firebaseadmin file update 2

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,16 +1,18 @@
-// firebaseAdmin.js
-import { initializeApp, cert } from 'firebase-admin/app';
+import admin from "firebase-admin";
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-};
+const { FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL } = process.env;
 
-if (!serviceAccount.projectId || !serviceAccount.clientEmail || !serviceAccount.privateKey) {
+if (!FIREBASE_PROJECT_ID || !FIREBASE_PRIVATE_KEY || !FIREBASE_CLIENT_EMAIL) {
   throw new Error("Missing Firebase Admin credentials in environment variables");
 }
 
-initializeApp({
-  credential: cert(serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: FIREBASE_PROJECT_ID,
+    private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: FIREBASE_CLIENT_EMAIL,
+  }),
 });
+
+export default admin;
