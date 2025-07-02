9595f56e204dcb7b5bee6af8765a33086c8210e2
unknown <xeno035>
2025-07-02 11:29:33 +0530

    privatekey modified

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -6,7 +6,7 @@ const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 initializeApp({
